9041ec15a0ab55dcc3839c1c4905ed90910b821a
collinsturi <collinsturi>
2024-07-21 16:08:41 +0300

    Feat: Added the b2c validation service

--- src/main/java/com/library/tanda/tandadarajaintegration/utility/ValidationService.java
+++ src/main/java/com/library/tanda/tandadarajaintegration/utility/ValidationService.java
@@ -1,5 +1,6 @@
 package com.library.tanda.tandadarajaintegration.utility;
 
+import com.library.tanda.tandadarajaintegration.Data.B2CRequest;
 import com.library.tanda.tandadarajaintegration.Entities.GwRequest;
 import org.springframework.stereotype.Service;
 
@@ -23,5 +24,13 @@ public class ValidationService {
                 validateAmount(gwRequest.getAmount()) &&
                 validateMobileNumber(gwRequest.getMobileNumber()) &&
                 gwRequest.getStatus() != null && !gwRequest.getStatus().isEmpty();
+    }
+    public boolean validateB2CRequest(B2CRequest b2CRequest) {
+        return b2CRequest != null &&
+                b2CRequest.getShortCode() != null && !b2CRequest.getShortCode().isEmpty() &&
+                b2CRequest.getCommandID() != null && !b2CRequest.getCommandID().isEmpty() &&
+                b2CRequest.getAmount() != null && validateAmount(Double.parseDouble(b2CRequest.getAmount())) &&
+                b2CRequest.getPartyA() != null && validateMobileNumber(b2CRequest.getPartyA()) &&
+                b2CRequest.getPartyB() != null && !b2CRequest.getPartyB().isEmpty();
     }
 }
